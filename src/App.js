602c0a6b2486b0d5ee1dc2aaf380cb1257e272af
Piyush <piyushguptaa2z123@gmail.com>
2022-01-17 02:10:18 +0530

    added dependency

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import "./App.css";
 import Layout from "./components/Layout";
-import Navbar from "./components/Navbar";
 import { getData } from "./utils/getData";
 
 function App() {
